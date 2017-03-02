fd4dd8e17cb5e351ebb2764758350510efec35ad
Tharak Krishnan <tharak.krishnan@gmail.com>
2017-03-02 10:45:36 -0800

    working pipeline

---
+++ CarND-Vehicle-Detection-P5/classifier.py
@@ -0,0 +1,83 @@
+import cv2
+import glob
+import time
+import pickle
+import numpy as np
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+from features import extract_features
+
+# Read in car and non-car images
+car_images = glob.glob('vehicles/*/*.png')
+non_car_images = glob.glob('non-vehicles/*/*.png')
+
+cars = []
+notcars = []
+for image in car_images:
+        cars.append(image)
+
+for image in non_car_images:
+        notcars.append(image)
+
+# TODO play with these values to see how your classifier
+# performs under different binning scenarios
+
+orient = 9
+pix_per_cell = 8
+cell_per_block = 2
+spatial_size = 32
+hist_bins = 32
+
+car_features = extract_features(cars, color_space='YCrCb', spatial_size=(spatial_size, spatial_size),
+                        hist_bins=hist_bins, hog_channel='ALL')
+notcar_features = extract_features(notcars, color_space='YCrCb', spatial_size=(spatial_size, spatial_size),
+                        hist_bins=hist_bins, hog_channel='ALL')
+
+# Create an array stack of feature vectors
+X = np.vstack((car_features, notcar_features)).astype(np.float64)                        
+# Fit a per-column scaler
+X_scaler = StandardScaler().fit(X)
+# Apply the scaler to X
+scaled_X = X_scaler.transform(X)
+
+# Define the labels vector
+y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+
+# Split up data into randomized training and test sets
+rand_state = np.random.randint(0, 100)
+X_train, X_test, y_train, y_test = train_test_split(
+    scaled_X, y, test_size=0.2, random_state=rand_state)
+
+print('Using spatial binning of:',spatial_size,
+    'and', hist_bins,'histogram bins')
+print('Feature vector length:', len(X_train[0]))
+# Use a linear SVC 
+svc = LinearSVC()
+# Check the training time for the SVC
+t=time.time()
+svc.fit(X_train, y_train)
+t2 = time.time()
+print(round(t2-t, 2), 'Seconds to train SVC...')
+# Check the score of the SVC
+print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+# Check the prediction time for a single sample
+t=time.time()
+n_predict = 10
+print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
+print('For these',n_predict, 'labels: ', y_test[0:n_predict])
+t2 = time.time()
+print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
+
+dist_pickle = {}
+dist_pickle["svc"] = svc
+dist_pickle["scaler"] = X_scaler
+dist_pickle["orient"] = orient
+dist_pickle["pix_per_cell"] = pix_per_cell
+dist_pickle["cell_per_block"] = cell_per_block
+dist_pickle["spatial_size"] = spatial_size
+dist_pickle["hist_bins"] = hist_bins
+pickle.dump( dist_pickle, open("svc_pickle.p", "wb" ))
+
