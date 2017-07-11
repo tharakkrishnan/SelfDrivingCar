4e954a5aabafb217c1256b625f410aaf85c55c59
Tharak Krishnan <tharak.krishnan@gmail.com>
2017-07-10 23:46:09 -0700

    fleshed out the PID ccontroller

--- 009-CarND-PIDControl/src/PID.cpp
+++ 009-CarND-PIDControl/src/PID.cpp
@@ -11,11 +11,22 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    Kp = Kp;
+    Ki = Ki;
+    Kd = Kd;
+
+    p_error = 1.0;
+    i_error = 0.0;
+    d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+    i_error += cte;
+    d_error = (cte - p_error);
+    p_error = cte;
 }
 
 double PID::TotalError() {
+    return -(Kp * p_error + Ki * i_error + Kd * d_error);
 }
 
